f1172ab6a8153aa0386ef48da67c6e2dc2694fb8
avnoto <alexandravnoto@gmail.com>
2020-09-17 18:33:47 -0700

    udpated server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
@@ -20,6 +21,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
